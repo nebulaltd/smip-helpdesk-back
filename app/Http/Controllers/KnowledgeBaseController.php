e3d2cb7657eb8207b80d97c926d2db36a70d7c90
Maksi Bajo <maksimiliano.bajo@nebula.al>
2024-07-02 14:03:43 +0200

    fixed knowledge base

--- app/Http/Controllers/KnowledgeBaseController.php
+++ app/Http/Controllers/KnowledgeBaseController.php
@@ -18,59 +18,18 @@ class KnowledgeBaseController extends Controller
 
     public function index(Request $request)
     {
-        $departments = Department::all();
-
-        if ($request->ajax()) {
-            $query = KnowledgeBase::with('department', 'user');
-            if ($request->has('kbCategory') && $request->kbCategory != 'all') {
-                $data = $query->where('department_id', '=', $request->kbCategory);
-            } elseif ($request->has('kbPinned') && $request->kbPinned != 'all') {
-                $data = $query->where('pinned', '=', $request->kbPinned);
-            } elseif ($request->has('kbStatus') && $request->kbStatus != 'all') {
-                $data = $query->where('status', '=', $request->kbStatus);
-            } else {
-                $data = $query;
-            }
-
-            return DataTables::of($data)
-                ->addColumn('content', function ($data) {
-                    return strip_tags(str_limit($data->content, 40));
-                })
-                ->addColumn('category', function ($data) {
-                    return $data->department->title;
-                })
-                ->addColumn('pinned_status', function ($data) {
-                    if ($data->pinned) {
-                        $checked = 'checked';
-                    } else {
-                        $checked = '';
-                    }
-                    $val = '<div class="switchToggle">
-                                        <input type="checkbox" class="pinned-class" id="switch-' . $data->id . '" data-id="' . $data->id . '"' . $checked . '>
-                                        <label for="switch-' . $data->id . '" data-id="' . $data->id . '">Toggle</label>
-                                    </div>';
-                    return $val;
-                })->addColumn('kb_status', function ($data) {
-                    if ($data->status == 0) {
-                        return '<span class="badge badge-warning">' . __('lang.unpublished') . '</span>';
-                    } else {
-                        return '<span class="badge badge-success">' . __("lang.published") . '</span>';
-                    }
-                })
-                ->addColumn('created_by', function ($data) {
-                    return $data->user->name;
-                })
-                ->addColumn('action', function ($data) {
-                    $editRoute = route('knowledge-base-edit.edit', $data->id);
-                    $value = '<a href="' . $editRoute . '" class="btn btn-primary btn-sm" title="Edit"><i class="fa fa-pencil"></i> </a>
-                              <button type="button" data-id="' . $data->id . '" data-toggle="modal" data-target="#DeleteDataModal" class="btn btn-danger btn-sm" id="getDataDeleteId" title="Delete"><i class="la la-trash-o"></i></button>';
-
-                    return $value;
-                })
-                ->rawColumns(['pinned_status', 'kb_status', 'action'])->make(true);
+        $query = KnowledgeBase::with('department', 'user');
+        if ($request->has('kbCategory') && $request->kbCategory != 'all') {
+            $data = $query->where('department_id', '=', $request->kbCategory);
+        } elseif ($request->has('kbPinned') && $request->kbPinned != 'all') {
+            $data = $query->where('pinned', '=', $request->kbPinned);
+        } elseif ($request->has('kbStatus') && $request->kbStatus != 'all') {
+            $data = $query->where('status', '=', $request->kbStatus);
+        } else {
+            $data = $query;
         }
 
-        return view('kb.index', compact('departments'));
+        return $this->successResponse($data);
     }
 
     public function store(Request $request)
